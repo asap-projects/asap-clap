82a1e46b09632208e3a291c1dd1fb9629b5e3dd8
abdes <abdes@users.noreply.github.com>
2022-08-19 20:48:14 +0000

    deploy: 7c095829854568306ad4a02c7996508addd548b8

--- asap_clap_master/html/_static/documentation_options.js
+++ asap_clap_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0 (a5faa22f0f0d)',
+    VERSION: '1.0.0 (7c0958298545)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
